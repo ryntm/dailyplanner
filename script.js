df8974f0b07b9dfd6966da5e39530b366e031a56
Ryan Tam <ryanjtam@gmail.com>
2020-02-11 01:26:59 -0800

    got the object values to display the value in each field.

--- script.js
+++ script.js
@@ -1,47 +1,93 @@
 $(document).ready(function() {
 
     var currentHour = moment().format('HH')
-    var tasks = JSON.parse(localStorage.getItem("dailytasks"));
+    // var tasks = JSON.parse(localStorage.getItem("dailytasks"));
     
-    if (tasks == undefined) {
-        tasks = {
-            "9": "",
-            "10": "",
-            "11": "",
-            "12": "",
-            "13": "",
-            "14": "",
-            "15": "",
-            "16": "",
-            "17": "",
-        }
-    }
+    // if (tasks == undefined) {
+    //     tasks = {
+    //         "9": "",
+    //         "10": "",
+    //         "11": "",
+    //         "12": "",
+    //         "13": "",
+    //         "14": "",
+    //         "15": "",
+    //         "16": "",
+    //         "17": "",
+    //     }
+    // }
 
-    console.log(tasks)
+    // console.log(tasks)
+
+    
 
     $('.saveBtn').on('click', function(){
-        var time = $(this).parent().attr("id");
+        var time = $(this).siblings(".description").attr("id");
         var task = $(this).siblings(".description").val().trim();
-        console.log(task);
-        localStorage.setItem(time, JSON.stringify(task));
+        tasks[time] = task
+        localStorage.setItem(tasks, JSON.stringify(tasks));
     })
 
     function getTasks() {
-        var tasks = JSON.parse(localStorage.getItem("time9"));
-        console.log(tasks);
-        $(".description").html()
-        // for (var i = 9; i < Object.keys(localStorage).length+9; i++) {
-        //     console.log(Object.keys(localStorage))
-        //     var tasks = JSON.parse(localStorage.getItem("time"+i));
-        //     console.log(tasks)
-        //     // console.log(localStorage."time"+9)
+        var tasks = JSON.parse(localStorage.getItem("tasks"||'{}'));
 
-        };
-    
+        if (tasks === null) {
+            tasks = 
+                    {hour9: "a",
+                    hour10: "b",
+                    hour11: "c",
+                    hour12: "d",
+                    hour13: "e",
+                    hour14: "f",
+                    hour15: "f",
+                    hour16: "g",
+                    hour17: "h"}
+            
+            localStorage.setItem('tasks', JSON.stringify(tasks))
+        }
+
+        console.log(Object.keys(tasks).length)
+        console.log()
+
+        for (var i = 0; i < Object.keys(tasks).length; i++) {
+           var hourText = "";
+           var j = i+9
+           var hourID = "hour" + j
+           console.log(tasks[hourID])
+           if (j > 12) {
+            hourText = j-12 + " PM"
+           } else if (j < 11) {
+            hourText = j + " AM"
+           } else {
+            hourText = j + " PM"
+           }
+
+            var textAreas = `
+           <div class="time-block"> 
+                <div class="row">
+                    <div class="hour">${hourText}</div>
+                    <textarea class="description" id="${hourID}">${tasks[hourID]}</textarea>
+                    <button class="saveBtn">save</button>
+                </div>
+            </div>`
+            $(".container").append(textAreas)
+        }
+
+        // showTasks();
+    };
+
+    // function showTasks () {
+    //     if (tasks !== null) {
+    //         for (var i = 0; i < tasks.length; i++) {
+    //             var j = i+9;
+    //         $("#"`${j}`).text(tasks[i]);
+    //         }
+    //     }
+    // }
 
     //function for comparing the time so that we may color the planner based on the time. 
 
 
-    // getTasks();
+    getTasks();
 
 })
